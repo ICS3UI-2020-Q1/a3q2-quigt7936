51d2b6b0415fb04825d5963660cd14f90fcef794
Thomas Quigley <quigt7936@wrdsb.ca>
2020-10-01 15:15:08 +0000

    Answered question

--- Main.java
+++ Main.java
@@ -1,15 +1,39 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * Help the user classify an animal
+ * @author Thomas Quigley
  */
 public class Main {
-
-  /**
-   * The method that is executed when you hit the run button.
-   * @param args the command line arguments
-   */
   public static void main(String[] args) {
-    // TODO code application logic here
+    Scanner input = new Scanner(System.in);
+    // ask if it has feathers or not
+    System.out.println("Does the animal have feathers?");
+    String feather = input.nextLine();
+    
+    if (feather.equals("yes")) {
+      // the animal is a bird, ask if it swims or not
+      System.out.println("Does the animal swim?");
+      String swims = input.nextLine();
+      
+      if (swims.equals("yes")) {
+        // the animal is a duck
+        System.out.println("This is a duck");
+      } else {
+        // the animal is a hen
+        System.out.println("This is a hen");
+      }
+    } else {
+      // the animal is a reptile, ask if it has legs or not
+      System.out.println("Does the animal have legs?");
+      String legs = input.nextLine();
+      if (legs.equals("yes")) {
+        // the animal is a lizard
+        System.out.println("This is a lizard");
+      } else {
+        // the animal is a snake
+        System.out.println("This is a snake");
+      }
+    }
     
   }
 }
